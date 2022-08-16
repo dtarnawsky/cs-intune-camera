41562811edef6cf1128ffded288f0106aada0335
Damian Tarnawsky <damian@ionic.io>
2022-08-16 08:33:20 -0700

    Initial Commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IntuneMAM } from '@ionic-enterprise/intune';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  version = null;
+
+  constructor(private router: Router) { }
+
+  async ionViewDidEnter() {
+    this.version = (await IntuneMAM.sdkVersion()).version;
+  }
+
+  async loginAndEnroll() {
+    await IntuneMAM.loginAndEnrollAccount();
+    const user = await IntuneMAM.enrolledAccount();
+    if (user.upn) {
+      console.log('Got user, going home');
+      this.router.navigate(['/home']);
+    }
+  }
+
+  async login() {
+    const authInfo = await IntuneMAM.acquireToken({
+      scopes: ['https://graph.microsoft.com/.default'],
+    });
+
+    console.log('Got auth info', authInfo);
+
+    await IntuneMAM.registerAndEnrollAccount({
+      upn: authInfo.upn,
+    });
+
+    const user = await IntuneMAM.enrolledAccount();
+    console.log('user', user);
+    if (user.upn) {
+      console.log('Got user, going home');
+      this.router.navigate(['/home']);
+    } else {
+      console.log('No user, logging in');
+      this.router.navigate(['/login']);
+    }
+  }
+
+  async showConsole() {
+    await IntuneMAM.displayDiagnosticConsole();
+  }
+
+}
